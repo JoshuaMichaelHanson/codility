71605fd7ad86dccbf14bbbf1f652ca5bef4b5b00
Josh Hanson <hanson.joshua@mayo.edu>
2022-06-23 16:44:06 -0500

    wip(Big O) More big O stuff and refactor files

--- src/test/java/com/hansonslogic/CtheCInterview/BigO/BigOTest.java
+++ src/test/java/com/hansonslogic/CtheCInterview/BigO/BigOTest.java
@@ -4,6 +4,8 @@ import com.hansonslogic.CtheCInterview.bigO.BigO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BigOTest {
 
     @Test
@@ -22,5 +24,42 @@ class BigOTest {
         BigO.multiplyRunTimes(A, B);
     }
 
+    @Test
+    @DisplayName("Two to the N or 2^N")
+    void run3() {
+        System.out.println(BigO.twoN(10));
+        assertEquals(512, BigO.twoN(10));
+    }
 
+    @Test
+    @DisplayName("Sum the array then product of the array")
+    void run4() {
+        BigO.sumArrayThenProductOfArray(new int[]{3, 8, 9, 7, 6});
+    }
+
+    @Test
+    @DisplayName("Is Prime?")
+    void run5() {
+       System.out.println(BigO.isPrime1(1939398789));
+       System.out.println(BigO.isPrime2(7919));
+    }
+
+    @Test
+    @DisplayName("All permutations of a string")
+    void run6() {
+        BigO.printPermutations("Josh", "--> ");
+    }
+
+    @Test
+    @DisplayName("Fibonacci Recursion")
+    void run7() {
+        System.out.println(BigO.fibonacciRecursion(10));
+    }
+
+    @Test
+    @DisplayName("Fibonacci Recursion with Memoization")
+    void run8() {
+        int[] memo = new int[10 + 1];
+        System.out.println(BigO.fibonacciRecursionWithMemoization(10, memo));
+    }
 }
